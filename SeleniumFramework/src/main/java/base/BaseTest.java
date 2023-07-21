b25d09ce767e97ffb9cb613ee77223a1e55c3593
brentgoktas <107077531+brentgoktas@users.noreply.github.com>
2023-07-21 07:23:49 -0400

    Fist Commit

---
+++ SeleniumFramework/src/main/java/base/BaseTest.java
@@ -0,0 +1,43 @@
+package base;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class BaseTest {
+	public static WebDriver driver;
+	public ExtentSparkReporter sparkReporter;
+	public ExtentReports extent;
+	public ExtentTest logger;
+
+	
+@BeforeTest
+public void beforeTestMethod()
+
+{
+	sparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") + File.separator + "rep" + File.separator + "SDETADDAExtentReport.html");
+	extent = new ExtentReports();
+	extent.attachReporter(sparkReporter);
+	sparkReporter.config().setTheme(Theme.DARK);
+	extent.setSystemInfo("HostName", "HEL8");
+	extent.setSystemInfo("UserName", "root");
+	sparkReporter.config().setDocumentTitle("Automation Report");
+	sparkReporter.config().setReportName("Automation Tests Results by SDET ADDA");
+
+}
+}
+
+	@BeforeMethod
+	@AfterMethod
+	@AfterTest
+	
+	
+	
+}
